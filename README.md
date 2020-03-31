10d2863b550dfa3dc52911e6216ec43998d2d4dc
Michael Wittig <michael@widdix.de>
2020-03-31 10:45:13 +0200

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# SQS queue monitoring
+
+Adds alarms to monitor SQS queue length and message age, and forwards them to Slack managed by [marbot](https://marbot.io/).
+
+## Usage
+
+1. Create a new directory
+2. Within the new directory, create a file `main.tf` with the following content:
+```
+provider "aws" {}
+
+module "marbot-monitoring-sqs-queue" {
+  source   = "marbot-io/marbot-monitoring-sqs-queue/aws"
+  #version = "x.y.z"         # we recommend to pin the version
+
+  endpoint_id = "" # to get this value, select a Slack channel where marbot belongs to and send a message like this: "@marbot show me my endpoint id"
+  queue_name  = "" # the queue name
+}
+```
+3. Run the following commands:
+```
+terraform init
+terraform apply
+```
+
+## Update procedure
+
+1. Update the `version`
+2. Run the following commands:
+```
+terraform get
+terraform apply
+```
+
+## License
+All modules are published under Apache License Version 2.0.
+
+## About
+A [marbot.io](https://marbot.io/) project. Engineered by [widdix](https://widdix.net).
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "endpoint_id" {
+  type        = string
+  description = "Your marbot endpoint ID (to get this value: select a Slack channel where marbot belongs to and send a message like this: \"@marbot show me my endpoint id\")."
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Turn the module on or off"
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "queue_name" {
+  type        = string
+  description = "The SQS queue name that you want to monitor."
+}
+
+variable "approximate_age_of_oldest_message_threshold" {
+  type        = number
+  description = "The maximum age (in seconds) of a message in the queue (set to 0 to disable)."
+  default     = 600
+}
+
+variable "approximate_number_of_messages_visible_threshold" {
+  type        = number
+  description = "The maximum number of messages in the queue waiting for processing (set to 0 to disable)"
+  default     = 10
+}
+
+variable "stage" {
+  type        = string
+  description = "marbot stage (never change this!)."
+  default     = "v1"
+}
